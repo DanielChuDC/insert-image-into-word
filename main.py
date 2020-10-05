7614c9b342452c7d775a84680d88a7af285a57e6
danielchu <52316624+DanielChuDC@users.noreply.github.com>
2020-10-06 01:07:19 +0800

    second commit

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 # For reading file name
 import glob
 print(glob.glob('.'))
+# For getting the full path
+import pathlib
+print(pathlib.Path(__file__).parent.absolute())
+
 
 myList=[]
 # initializing split word 
 spl_word = '/'
 myListWithoutSlash=[]
-for file_name in glob.iglob('./imgs/*.jpeg', recursive=True):
+for file_name in glob.iglob(str(pathlib.Path(__file__).parent.absolute())+ '/imgs/*.jpeg', recursive=True):
   print(file_name)
   myList.append(file_name)
   # using split() 
   # Get String after substring occurrence 
   res = file_name.split('/')
+  res= [item for item in res if "jpeg" in item or "JPEG" in item or "png" in item or "PNG" in item or "jpg" in item or "JPG" in item]
   myListWithoutSlash.append(res)
 
 # for python-docs package
@@ -37,7 +42,7 @@ for i in range(len(myList)):
   print(i)
   print(myListWithoutSlash[i])
   row_cells = table.add_row().cells
-  row_cells[2].text = myListWithoutSlash[i][2]
+  row_cells[2].text = myListWithoutSlash[i]
   p = row_cells[3].add_paragraph()
   r = p.add_run()
   r.add_picture(myList[i],width=Cm(4.0), height=Cm(4))
--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-pip install -r requirements.txt
-python main.py
+if [[ "$OSTYPE" == "darwin"* ]]; 
+then
+  # Mac OSX
+  pip install -r requirements.txt
+  python main.py
+elif [[ "$OSTYPE" == "win32" ]]; 
+then
+  pip install -r requirements.txt
+  py main.py
+else
+  pip install -r requirements.txt
+  py main.py
+fi
 
